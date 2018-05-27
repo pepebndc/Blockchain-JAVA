90eaba77e949b7439bf9681140378e04f477175b
Pepe Blasco <pepeblascondc@gmail.com>
2018-05-27 12:14:48 +0200

    Revert "refactor android app"
    This reverts commit a0dec697f4925c55b7acd1814d7f12cea77c5d35.

---
+++ Android app/Blockchain/feature/src/main/java/es/upv/joblanu1/blockchain/feature/User.java
@@ -0,0 +1,67 @@
+package es.upv.joblanu1.blockchain.feature;
+
+
+import java.io.Serializable;
+import java.security.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Random;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 5950169519310163575L;
+    private String name;
+    private String address;
+    private PublicKey publicKey;
+
+    User(String name, String address, PublicKey publicKey) throws NoSuchAlgorithmException {
+
+        this.name = name;
+        this.address = address;
+        this.publicKey = publicKey;
+
+    }
+
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the publicKey
+     */
+    public PublicKey getPublicKey() {
+        return publicKey;
+    }
+
+    /**
+     * @param publicKey the publicKey to set
+     */
+    public void setPublicKey(PublicKey publicKey) {
+        this.publicKey = publicKey;
+    }
+}
